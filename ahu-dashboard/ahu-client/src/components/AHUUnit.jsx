e35f6862ab35e0cf55644f096dfd863e956bda36
iamgauravkha <gauravk@oplify.in>
2025-02-12 16:56:04 +0530

    update

--- ahu-dashboard/ahu-client/src/components/AHUUnit.jsx
+++ ahu-dashboard/ahu-client/src/components/AHUUnit.jsx
@@ -3,24 +3,17 @@ import { OrbitControls, useGLTF } from "@react-three/drei";
 
 function FanModel() {
   const { scene } = useGLTF("/fan_3d_model.glb");
-  return (
-    <primitive
-      object={scene}
-      scale={12}
-      position={[10, -9, -60]}
-      rotation={[0, -0.8 , 0]}
-    />
-  );
+  return <primitive object={scene} scale={10} position={[10, -7, -40]} />;
 }
 
 export default function AHUUnit() {
   return (
     <Canvas
-      camera={{ position: [50, 50, 100], fov: 35 }}
+      camera={{ position: [50, 50, 100], fov: 40 }}
       className=" bg-gray-300"
     >
-      <ambientLight intensity={0.5} />
-      <directionalLight position={[10, 10, 10]} />
+      <ambientLight intensity={1} />
+      <directionalLight position={[15, 10, 15]} />
       <FanModel />
       <OrbitControls enableZoom enableRotate enablePan />
     </Canvas>
